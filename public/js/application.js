e4d1c7e35485b8b7202968450e9e2b0a46e20f97
Jay Davis & Scott Enriquez <pair+planarian.sjenriquez@devbootcamp.com>
2015-01-23 18:00:55 -0500

    add js to create new choice divs with unique input names.

--- public/js/application.js
+++ public/js/application.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var choiceCounter = 0;
   $('.add_question').click(function() {
     $('.question').css("visibility", "visible");
     $('.add_choice').css("visibility", "visible");
@@ -10,5 +11,18 @@ $(document).ready(function() {
   $('.add_choice').click(function(){
     var $choice = $('.choice');
     $choice.first().clone().appendTo( ".choices" ).css("visibility", "visible");
+    $('input').last().prop('name', "choices[choice_" + choiceCounter + "]");
+    choiceCounter++;
+
   })
+
+
+
+
 });
+
+  // $(".add_choice").click(function(){
+  //   var $choice = $('.choice');
+  //   $choice.first().clone().prop('name', 'ham').appendTo( ".choices" ).css("visibility", "visible");
+  //   $(this).prop('type', 'date');
+  // });
