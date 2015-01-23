db8981d5dc6520d86f3de82dbb54c5f2b53ec4bf
Jonathan Cho <joncho26@gmail.com>
2015-01-23 16:23:15 -0500

    added a signup view and link for users

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,11 +3,22 @@ get '/' do
 end
 
 get '/login' do
+  erb :'auth/login'
+end
+
+post '/login' do
 
 end
 
 get '/signup' do
+  erb :'auth/signup'
+end
+
+post '/signup' do
 
 end
 
+get '/logout' do
+
+end
 
